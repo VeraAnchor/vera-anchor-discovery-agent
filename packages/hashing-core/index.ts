13d659cbe47890fe0462d81d32aca03fe6dc279f
FERRIS <richard.cheze1@gmail.com>
2026-06-01 15:50:14 -0600

    Initialized core hashing lib for evidence reconstruction based on existing VA primitives.

---
+++ packages/hashing-core/index.ts
@@ -0,0 +1,18 @@
+export { hashJson, hashUtf8, hashRaw } from "./hashFactory.js";
+
+export {
+  CANONICAL_JSON_ID,
+} from "./types.js";
+
+export type {
+  CanonId,
+  DigestEncoding,
+  HashAlg,
+  HashFactoryContractId,
+  FrameId,
+  HashKind,
+  HashEnvelopeJsonV1,
+  HashEnvelopeBytesV1,
+  HashEnvelopeV1,
+  HashMaterialInclude,
+} from "./types.js";
